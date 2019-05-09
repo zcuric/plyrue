77c7f553cf710bb109dfcf3304838d88d34a4b7b
zcuric <zcuric@gmail.com>
2019-05-09 16:22:29 +0200

    Implement Video and VideoEmbed components

---
+++ src/components/Video.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <video id="player" v-bind="attributes">
+      <slot/>
+      <template v-if="sources">
+        <source v-for="source in sources" v-bind="source" :key="source.src">
+      </template>
+      <template v-if="captions">
+        <track
+          v-for="caption in captions"
+          v-bind="caption"
+          :key="caption.label"
+          kind="captions"
+          default
+        >
+      </template>
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    sources: {
+      type: Array,
+      default: () => []
+    },
+    captions: {
+      type: Array,
+      default: () => []
+    },
+    autoplay: {
+      type: Boolean,
+      default: false
+    },
+    crossorigin: {
+      type: Boolean,
+      default: false
+    },
+    controls: {
+      type: Boolean,
+      default: false
+    },
+    loop: {
+      type: Boolean,
+      default: false
+    },
+    muted: {
+      type: Boolean,
+      default: false
+    },
+    preload: {
+      type: String,
+      default: "none"
+    },
+    playsinline: {
+      type: Boolean,
+      default: false
+    },
+    poster: {
+      type: String,
+      default: null
+    },
+    src: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    attributes() {
+      const attributes = { ...this.$props };
+      delete attributes.sources;
+      delete attributes.captions;
+      return attributes;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/VideoEmbed.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="plyr__video-embed" id="player">
+      <iframe :src="source" v-bind="$attrs"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+const YOUTUBE_VIDEO = `https://www.youtube.com/watch?v=`;
+const YOUTUBE_EMBED = `https://www.youtube.com/embed/`;
+
+export default {
+  inheritAttrs: false,
+  props: {
+    src: {
+      type: String, default: () => null
+    }
+  },
+  computed: {
+    source() {
+      const { src } = this;
+      // This fixes 
+      // `Refused to display 'src' in a frame because it set 'X-Frame-Options' to 'SAMEORIGIN'`.
+      // error
+      if (src.includes(YOUTUBE_VIDEO)) {
+        return src.replace(YOUTUBE_VIDEO, YOUTUBE_EMBED);
+      } 
+
+      return src;
+    }
+  }
+};
+</script>
+
