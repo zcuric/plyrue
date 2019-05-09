0c58d28e5614a880523c13fc7132db50ff98b3ee
zcuric <zcuric@gmail.com>
2019-05-09 16:25:42 +0200

    Create example in its own directory

--- example/main.js
+++ example/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import Plyrue from '../src/index';
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(Plyrue, {
+  config: {
+    autoplay: true
+  }
+});
 
 new Vue({
   render: h => h(App),
